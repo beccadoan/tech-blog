694d727e024f017abb27c85363471fda57e51eee
Becca Doan <rebecca.doan@yahoo.com>
2022-08-07 00:59:47 -0500

    added Post and Comment models

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-// const helpers = require('./utils/helpers')
+const helpers = require('./utils/helpers')
 const exphbs = require('express-handlebars')
-// const hbs = exphbs.create({ helpers })
-const hbs = exphbs.create({  })
+const hbs = exphbs.create({ helpers })
 
 const app = express();
 const session = require('express-session')
