e47607ee5d1368fce34ab54588fd9cff9f036037
Becca Doan <rebecca.doan@yahoo.com>
2022-08-07 14:17:41 -0500

    added login and signup

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     console.log('=====================');
@@ -63,12 +63,11 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_body: req.body.post_body,
-        // user_id: req.session.user_id
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
     .then(dbPostData => {res.json(dbPostData)})
     .catch(err => {
@@ -77,7 +76,7 @@ router.post('/', (req, res) => {
     })
 })
 
-router.put('/:id',(req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
         title: req.body.title
@@ -101,7 +100,7 @@ router.put('/:id',(req, res) => {
       });
   });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,11 +40,19 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-      if(req.session.loggedIn) {
-        res.redirect('/');
-        return;
-      }
+    //   if(req.session.loggedIn) {
+    //     res.redirect('/');
+    //     return;
+    //   }
       res.render('login')
+})
+
+router.get('/signup', (req, res) => {
+    //   if(req.session.loggedIn) {
+    //     res.redirect('/');
+    //     return;
+    //   }
+      res.render('signup')
 })
 
 module.exports = router;
